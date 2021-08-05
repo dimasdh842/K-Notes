72fc164c021ac46b53d4bb206f24146b4d391c41
dimasdh842 <penamas842@gmail.com>
2021-08-05 17:25:32 +0700

    deploy my react app

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -9,7 +9,6 @@ import {Provider} from 'react-redux'
 function App() {
   return (
     <Provider store={store}>
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossOrigin="anonymous"></link>
       <Router>
           <Route path="/" exact component={Dashboard}/>
           <Route path="/login" component={Login}/>
